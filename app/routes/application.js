c302541f4f5c7156d4751f736ba83985bc8cebe1
d3ah <diegoeauyonh@gmail.com>
2019-06-02 09:35:16 -0600

    Actualizando imagenes y listado de herramientas.

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,19 +4,57 @@ export default Route.extend({
     model() {
         return [{
             title: 'Nuestra Elección',
-            description: 'Nuestra Elección es una plataforma .....',
-            img: '01.png',
-            imgAlt: 'Lupa'
-        },{
-            title: 'Nuestra Elección',
-            description: 'Nuestra Elección es una plataforma .....',
-            img: '01.png',
-            imgAlt: 'Lupa'
-        }, {
-            title: 'Nuestra Elección',
-            description: 'Nuestra Elección es una plataforma .....',
-            img: '01.png',
-            imgAlt: 'Lupa'
+            img: 'altoparlante.png',
+            url: 'https://nuestraeleccion.gt',
+            description: '¡Haz propuestas o apoyalas! Las propuestas serán entregadas a las candidaturas.'
+        },
+        {
+            title: 'Candidatos gt',
+            img: 'cuaderno.png',
+            url: 'https://candidatos.gt',
+            description: 'Consulta los datos y compara los perfiles de las candidaturas en estas elecciones.'
+        },
+        {
+            title: '¿Por Quién Voto?',
+            img: 'brujula.png',
+            url: 'https://porquienvoto.org.gt/',
+            description: '¡Conoce qué partido político comparte tu ideología!'
+        },
+        {
+            title: '3 de 3',
+            img: 'carnet.png',
+            url: 'https://www.3de3.gt/',
+            description: '¡Conoce quiénes son las candidaturas cabales que presentaron sus tres declaraciones: patrimonial, e intereses y fiscal!'
+        },
+        {
+            title: 'Confirmado',
+            img: 'lupa.png',
+            url: 'https://www.confirmado.org/',
+            description: '¡Conoce qué datos y noticias son verdaderos y cuáles falsos!'
+        },
+        {
+            title: 'Yo voto Informado',
+            img: 'lapiz.png',
+            url: 'https://yovotoinformado.org/',
+            description: '¿Seguro que tu candidato/a  comparte tu postura? ¡Descúbrelo aquí!'
+        },
+        {
+            title: 'Elecciones Visibles',
+            img: 'silbato.png',
+            url: 'https://eleccionesvisibles.gt/',
+            description: '¡Conviértete en un observador electoral! ¡Cuidemos las elecciones!'
+        },
+        {
+            title: 'Mirador Electoral',
+            img: 'binoculares.png',
+            url: 'https://www.miradorelectoral.gt/',
+            description: '¿Cómo vamos con las elecciones? Conoce los datos aquí.'
+        },
+        {
+            title: 'Centro de Ubicación',
+            img: 'mapa.png',
+            url: 'https://endondevotas.tse.org.gt/',
+            description: '¿Ya sabes dónde te toca votar? Haz click aquí'
         }];
     }
 });
